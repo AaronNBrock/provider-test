7520dc71328f600f1a5f744c001697b25198e771
henry Gallo <gallohenry@gmail.com>
2020-03-23 13:33:14 -0400

    henry loves to help :)

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+  profile = "terraform_demo"
+}
+
+provider "aws" {
+  alias   = "test_provider"
+  version = "~> 2"
+  region  = "us-east-1"
+}
+
 module "provider-test" {
   source = "./modules/provider-test"
+  providers = {
+    aws = aws
+    aws.one = aws.test_provider
+    }
 }
--- modules/provider-test/main.tf
+++ modules/provider-test/main.tf
@@ -1,11 +1,8 @@
 provider "aws" {
-  alias   = "test_provider"
-  version = "~> 2"
-  region  = "us-east-1"
+  alias = "one"
 }
 
 resource "aws_s3_bucket" "b" {
-  provider = aws.test_provider
   bucket   = "my-tf-test-bucket-henry"
   acl      = "private"
 }
